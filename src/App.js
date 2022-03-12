b29d7cc182ed93628736c6d3c6d57f213c417471
Chris Kavanagh <ckava3@gmail.com>
2022-03-12 10:25:13 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Link, Route, Routes } from "react-router-dom";
+import ReactDOM from "react-dom";
+import Home from "./components/Home";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import * as classes from "./styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <section className={classes.wrapper}>
+      <h1 className={classes.top}>Welcome To A React App With Parcel</h1>
+      <p className={[classes.para, classes.text].join(" ")}>
+        ipsum dolor sit amet consectetur adipisicing elit. Odio excepturi
+        eligendi minima ab hic. Dolorem praesentium possimus ea, a consequuntur
+        assumenda animi enim accusamus cumque? Repellendus aliquid voluptates
+        enim. Maxime.
+      </p>
+      <p className={[classes.para2, classes.text].join(" ")}>
+        minima eligendi iure quas modi alias placeat inventore, cupiditate neque
+        architecto quam perferendis consectetur similique nulla repellendus
+        quisquam amet. Eum placeat nam commodi et veniam quisquam, architecto
+        amet adipisci.
+      </p>
+    </section>
+  );
+}
